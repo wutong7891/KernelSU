ef150ea8476eb1aee0acdadb2fadb71bc2be5e32
wutong7891 <3859276047@qq.com>
2026-09-25 01:32:33 +0800

    manager: Add wtlyf toolbox app

---
+++ manager/wtlyf-toolbox/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    alias(libs.plugins.agp.app)
+    alias(libs.plugins.lsplugin.apksign)
+}
+
+apksign {
+    storeFileProperty = "KEYSTORE_FILE"
+    storePasswordProperty = "KEYSTORE_PASSWORD"
+    keyAliasProperty = "KEY_ALIAS"
+    keyPasswordProperty = "KEY_PASSWORD"
+}
+
+android {
+    namespace = "com.wutong.yingcang"
+    compileSdk = 37
+
+    defaultConfig {
+        applicationId = "com.wutong.yingcang"
+        minSdk = 31
+        targetSdk = 37
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            vcsInfo.include = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+}
+
+base { archivesName.set("wtlyf") }
