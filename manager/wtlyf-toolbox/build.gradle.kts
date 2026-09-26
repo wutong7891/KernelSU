ab1cdf81955b82b850f607d8067bb73fa8456f15
wutong7891 <3859276047@qq.com>
2026-09-26 14:39:05 +0800

    toolbox: release version 1.1.0

--- manager/wtlyf-toolbox/build.gradle.kts
+++ manager/wtlyf-toolbox/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.wutong.yingcang"
         minSdk = 31
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
 
     buildTypes {
